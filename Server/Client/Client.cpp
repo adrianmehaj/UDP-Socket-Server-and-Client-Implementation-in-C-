1ab40e22ec508c7c75a01aa86acffd14707fdad9
adrianmehaj <adrianmehaj123@gmail.com>
2024-11-11 18:47:02 +0100

    Definimi i Portit si Variabel

--- Server/Client/Client.cpp
+++ Server/Client/Client.cpp
@@ -1,2 +1,10 @@
-// Client.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
+
+
+#pragma comment(lib, "Ws2_32.lib") // Link with Winsock library
+
+#define SERVER_IP "192.168.178.79" // Replace with your server's actual IP address
+#define PORT 8080
