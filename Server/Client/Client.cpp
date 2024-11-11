c8988156d412a7fc35a02479f65ce78c51b79054
adrianmehaj <adrianmehaj123@gmail.com>
2024-11-11 18:51:08 +0100

    Funksioni për dërgim dhe marrje të mesazhit

--- Server/Client/Client.cpp
+++ Server/Client/Client.cpp
@@ -8,3 +8,23 @@
 
 #define SERVER_IP "192.168.178.79" // Replace with your server's actual IP address
 #define PORT 8080
+
+
+void sendMessage(SOCKET sockfd, sockaddr_in& serverAddr, const std::string& message) {
+    int sentBytes = sendto(sockfd, message.c_str(), message.size(), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+    if (sentBytes == SOCKET_ERROR) {
+        std::cerr << "Failed to send message.\n";
+        return;
+    }
+
+    char buffer[1024];
+    int addrLen = sizeof(serverAddr);
+    int n = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0, (struct sockaddr*)&serverAddr, &addrLen);
+    if (n == SOCKET_ERROR) {
+        std::cerr << "Failed to receive server response.\n";
+        return;
+    }
+
+buffer[n] = '\0';
+std::cout << "Server response: " << buffer << std::endl;
+}
